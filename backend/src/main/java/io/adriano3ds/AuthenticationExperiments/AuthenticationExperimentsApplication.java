c9e654ab7da3f291ccb6e15a467963fc2707b31e
Adriano Junior <jr.adrianogm@gmail.com>
2024-01-26 20:06:12 -0300

    Add Spring Initializr files

---
+++ backend/src/main/java/io/adriano3ds/AuthenticationExperiments/AuthenticationExperimentsApplication.java
@@ -0,0 +1,13 @@
+package io.adriano3ds.AuthenticationExperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationExperimentsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/adriano3ds/AuthenticationExperiments/AuthenticationExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.adriano3ds.AuthenticationExperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
